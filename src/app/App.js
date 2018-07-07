8bc90d1c457f71e8885692f032ed4f7f559e2d90
Maken Hubbard <makhub26@outlook.com>
2018-07-07 12:42:59 -0500

    wip

--- src/app/App.js
+++ src/app/App.js
@@ -11,6 +11,8 @@ import Navbar from '../components/Navbar/Navbar';
 // import OrderSpa from '../components/OrderSpa/OrderSpa';
 import Register from '../components/Register/Register';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
+import fbConnection from '../firebaseRequest/connection';
+fbConnection();
 
 const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return (
