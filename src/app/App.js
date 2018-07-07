4782e47ceac0c5c00c27691cc42b47c2c9dc0435
Maken Hubbard <makhub26@outlook.com>
2018-07-07 13:43:03 -0500

    adding the links for the auth change

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import firebase from 'firebase';
 // import Fish from '../components/Fish/Fish';
 import Home from '../components/Home/Home';
 import Inventory from '../components/Inventory/Inventory';
@@ -49,15 +50,32 @@ const PublicRoute = ({ component: Component, authed, ...rest }) => {
 };
 
 class App extends React.Component {
-  state={
+  state = {
     authed: false,
   }
+
+  componentDidMount () {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount () {
+    this.removeListener();
+  }
+
   render () {
     return (
       <div className="App">
         <BrowserRouter>
           <div>
-            <Navbar />
+            <Navbar
+              authed={this.state.authed}
+            />
             <div className="container">
               <div className="row">
                 <Switch>
