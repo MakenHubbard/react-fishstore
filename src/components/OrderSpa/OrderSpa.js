d8deb19be0ac1fc0778c60f3fedd92b0ff475d7e
Maken Hubbard <makhub26@outlook.com>
2018-07-09 20:51:00 -0500

    order get request

--- src/components/OrderSpa/OrderSpa.js
+++ src/components/OrderSpa/OrderSpa.js
@@ -1,11 +1,41 @@
 import React from 'react';
+
+import orderRequests from '../../firebaseRequests/orders';
+import authRequests from '../../firebaseRequests/auth';
+import Orders from '../Order/Order';
 import './OrderSpa.css';
 
 class OrderSpa extends React.Component {
+  state = {
+    orders: [],
+  }
+
+  componentDidMount () {
+    orderRequests
+      .getRequest(authRequests.getUid())
+      .then((orders) => {
+        this.setState({orders});
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      });
+  }
+
   render () {
+    const orderComponents = this.state.orders.map((order) => {
+      return (
+        <Orders
+          key={order.id}
+        />
+      );
+    });
     return (
       <div className="OrderSpa">
-        <h1>OrderSpa</h1>
+        <h2>Orders</h2>
+        <button>New Order</button>
+        <ul>
+          {orderComponents}
+        </ul>
       </div>
     );
   }
